a8d90e84bc9b7cea48da46281849951ee2d8d8ad
Davide <davidedona03@gmail.com>
2024-03-23 14:42:12 +0100

    Updated comments

--- f1App/src/App.tsx
+++ f1App/src/App.tsx
@@ -19,28 +19,30 @@ export type RootStackParamList = {
 //Variabile utile al passaggio di parametri tra screen e lo spostamento tra essi
 export type RootStackNavigationProp = StackNavigationProp<RootStackParamList>;
 
-//Pagina start inserita qua per semplicità
+/*STARTING SCREEN component*/
 export const StartingScreen = () => {
-
   const navigation = useNavigation<RootStackNavigationProp>();//utilizzo parametri nel navigator
-
-  return (//Struttura di Starting Screen che setta l'immagine a tutto schermo con possibilità di tocco
+  return (
     <View style={styles.starting_container}>
-      <TouchableOpacity onPress={() => navigation.navigate('HomePage')} >
+      {/*Making the startingScreen clickable. By clicking it the user will get redirected to the HomePage.
+        By using replace instead of navigate we can make sure the user can't come back to the starting screen*/}
+      <TouchableOpacity onPress={() => navigation.replace('HomePage')} >
       <Image source={require('../img/starting-image.jpg')} style={styles.starting_image} />
       </TouchableOpacity>
     </View>
   );
 };
 
-//Struttura generale App > headerShown obbligatoria in quanto richiesta dal compilatore
+/*Structure for the navigation system of the app*/
 const App = () => {
   return (
-    //Sezione che si occupa della gestione del Navigator come i vari Screen settati
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="StartingScreen" component={StartingScreen}  options={{ headerShown: false }/* nasconde headre */ } />
+        {/*We are setting the headerShown to false, to hide the bar in the top of the screen for the navigation*/}
+        <Stack.Screen name="StartingScreen" component={StartingScreen}  options={{ headerShown: false }} />
         <Stack.Screen name='HomePage' component={HomePage} options={{ headerShown: false }} />
+        
+        {/*Here instead, the header will take you to the homePage*/}
         <Stack.Screen name='Schedule' component={Schedule} />
       </Stack.Navigator>
     </NavigationContainer>
@@ -49,7 +51,8 @@ const App = () => {
 
 export default App;
 
-//style semplici utili al setting della starting page
+
+//Styles used only for the starting screen
 const styles = StyleSheet.create({
   starting_container: {
     flex: 1,
@@ -57,9 +60,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   starting_image: {
-    width: Dimensions.get('window').width, // Larghezza dell'immagine pari alla larghezza dello schermo
-    height: Dimensions.get('window').height, // Altezza dell'immagine pari all'altezza dello schermo
-    resizeMode: 'cover', // Adatta l'immagine per coprire completamente l'area
+    width: Dimensions.get('window').width, // Width of the image is setted to the width of the screen
+    height: Dimensions.get('window').height, // Height of the image is setted to the height of the screen
+    resizeMode: 'cover', // resize the image to cover the whole screen
   },
 });
 
