e546e09ff76c30606bf29d0a21b9afadb46067cc
Blushi Andre <andreablushi@gmail.com>
2024-03-23 13:37:55 +0100

    Versione con strating page, base homepage e parte schedule

---
+++ f1App/src/HomePage.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RootStackParamList } from './App'; // Importa RootParamList da App.tsx
+
+type HomePageNavigationProp = NativeStackNavigationProp<RootStackParamList>;
+
+const HomePage = () => {
+
+  const navigation = useNavigation<HomePageNavigationProp>();
+  const goToSchedule = () => {
+    navigation.navigate('Schedule');
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to the Homepage</Text>
+      <Button title="Go to Schedule" onPress={goToSchedule} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color:'black',
+    marginBottom: 20,
+  },
+});
+
+export default HomePage;
---
+++ f1App/src/Schedule.tsx
@@ -0,0 +1,177 @@
+import React, { useEffect, useState } from 'react';
+import {
+  Button,
+  Image,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import {
+  Colors,
+} from 'react-native/Libraries/NewAppScreen';
+type Race = {
+  round: number,
+  raceName: string
+  Circuit: {
+    circuitName: string,
+    circuit: string,
+    Location: {
+      country: string,
+    }
+  }
+  date: string
+}
+
+type Props = {
+  darkMode: boolean
+  race: Race
+}
+
+function RaceSchedule(props: Props): React.JSX.Element {
+  // import prop
+  const isDarkMode = props.darkMode;
+  const race = props.race;
+
+  // date formatting
+  const day = race.date.slice(8, 10);
+  const month = race.date.slice(5, 7);
+  const year = race.date.slice(0, 4);
+  const date = day + "/" + month + "/" + year;
+
+  // dark theme
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    color: isDarkMode ? '#bfbfbf' : '#262626',
+  };
+  
+  return (
+    <View style={[styles.raceScheduleContainer, {flex: 1, marginVertical: 7}]}>
+      <View style={{shadowColor: 'black', elevation: 3, shadowOffset: {height: 2, width: 2}, shadowRadius: 3, shadowOpacity: 1}}>
+        <Image source={require('../Formula1-Images-API/public/countries/italy.png')} style={[{resizeMode:'contain',  width: 60, height:60,  flex: 1}]}></Image>
+      </View>
+      <View style={[{marginHorizontal: 10, flex: 12},]}>
+        <Text style={[styles.sectionDescription, styles.highlight, backgroundStyle, {fontSize: 16} ]}>Round {race.round}</Text>
+        <Text style={[styles.sectionDescription, backgroundStyle]}>{race.raceName}</Text>
+        <Text style={[styles.sectionDescription, backgroundStyle]}>{race.Circuit.circuitName}</Text>
+      </View>
+      <Text style={[{textAlign: 'right', flex: 5, marginRight: 10},  styles.sectionDescription, styles.highlight, backgroundStyle,]}>{date}</Text>
+      
+  </View>
+  );
+};
+
+  
+function Schedule(): React.JSX.Element {
+  // hooks
+  const [darkMode, setDarkMode] = useState(useColorScheme() === 'dark');
+  const [race, setRace] = useState<Race[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  // control variables
+  const isDarkMode = darkMode;
+  let year = 2023;
+  let apiUrl = "https://ergast.com/api/f1/"+ year +".json";
+
+  // dark theme 
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    color: isDarkMode ? 'white' : 'black',
+  };
+
+  // data fetching
+  const getRace =  async() => {
+    try {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      setRace(data.MRData.RaceTable.Races);
+      console.log("retrieving data");
+    } catch (error){
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // fetch data at the start of the page
+  useEffect(() => {
+    getRace();
+  }, []);
+
+  // theme manager
+  const switchTheme= () => {
+    darkMode ? setDarkMode(false) : setDarkMode(true);
+  }
+  
+  return (
+      <SafeAreaView style={[backgroundStyle, {flex: 11}]}>
+        <View style={[{backgroundColor: backgroundStyle.backgroundColor}, styles.topBar]}>
+          <Text style={[styles.topBarText, {color: backgroundStyle.color}]}>Schedule</Text>
+          <View style={{marginLeft: 20, flex: 1,}}>
+            <Text style={[styles.topBarYear, {color: backgroundStyle.color}]}>Year: {year}</Text>
+          </View>
+        </View>
+        <View style={[{flex: 10}]}>
+          <ScrollView>
+            {race.map( race => <View key={race.round}>
+              <RaceSchedule darkMode={darkMode} race={race}></RaceSchedule>
+            </View>)}
+          </ScrollView>
+          
+          <Button onPress={switchTheme} title='switch theme'></Button>
+        </View>
+      </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    fontSize: 14,
+    fontWeight: '400',
+    
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  topBarText: {
+    flex: 1,
+    fontSize: 30,
+    textAlignVertical: 'center',
+    marginHorizontal: 10,
+    fontWeight: '700',
+    color: 'white'
+    
+  }, topBar: {
+    height: 70,  
+    flex:1,
+    flexDirection: 'row'
+
+  },
+  topBarYear : {
+    flex: 1,
+    textAlignVertical: 'center',
+    textAlign: 'right',
+    paddingRight: 20,
+    fontSize: 20,
+    fontWeight: '600'
+  },
+  raceScheduleContainer: { 
+    marginHorizontal: 10,
+    flexDirection: 'row',
+    flex: .1,
+    alignItems: 'center',
+  }, 
+
+});
+
+export default Schedule;
