0dd59711b1eed8472f7dffdb2e95cb199de47b12
Davide <davidedona03@gmail.com>
2024-03-25 21:35:34 +0100

    Trying to implement dynamic images

---
+++ f1App/utils/ImagesDB.js
@@ -0,0 +1,91 @@
+const Images = [
+    {
+        "drivers-front": [
+          {
+            "name": "albon",
+            "src": require("../img/drivers/albon_front.png"),
+          },
+          {
+            "name": "alonso",
+            "src": require("../img/drivers/alonso_front.png"),
+          },
+          {
+            "name": "bottas",
+            "src": require("../img/drivers/bottas_front.png"),
+          },
+          {
+            "name": "gasly",
+            "src": require("../img/drivers/gasly_front.png"),
+          },
+          {
+            "name": "hamilton",
+            "src": require("../img/drivers/hamilton_front.png"),
+          },
+          {
+            "name": "hulkenberg",
+            "src": require("../img/drivers/hulkenberg_front.png"),
+          },
+          {
+            "name": "magnussen",
+            "src": require("../img/drivers/kevin_magnussen_front.png"),
+          },
+          {
+            "name": "leclerc",
+            "src": require("../img/drivers/leclerc_front.png"),
+          },
+          {
+            "name": "verstappen",
+            "src": require("../img/drivers/verstappen_front.png"),
+          },
+          {
+            "name": "norris",
+            "src": require("../img/drivers/norris_front.png"),
+          },
+          {
+            "name": "ocon",
+            "src": require("../img/drivers/ocon_front.png"),
+          },
+          {
+            "name": "perez",
+            "src": require("../img/drivers/perez_front.png"),
+          },
+          {
+            "name": "piastri",
+            "src": require("../img/drivers/piastri_front.png"),
+          },
+          {
+            "name": "ricciardo",
+            "src": require("../img/drivers/ricciardo_front.png"),
+          },
+          {
+            "name": "russel",
+            "src": require("../img/drivers/russel_front.png"),
+          },
+          {
+            "name": "sainz",
+            "src": require("../img/drivers/sainz_front.png"),
+          },
+          {
+            "name": "sargeant",
+            "src": require("../img/drivers/sargeant_front.png"),
+          },
+          {
+            "name": "stroll",
+            "src": require("../img/drivers/stroll_front.png"),
+          },
+          {
+            "name": "tsunoda",
+            "src": require("../img/drivers/tsunoda_front.png"),
+          },
+          {
+            "name": "zhou",
+            "src": require("../img/drivers/zhou_front.png"),
+          },
+        ],
+        "drivers-side": [
+        ],
+        "circuits": [
+        ]
+      }
+      
+];
