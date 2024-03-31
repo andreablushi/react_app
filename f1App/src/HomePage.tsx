deb5837b97613471733cb86c040fa9602f14a7df
Blushi Andre <andreablushi@gmail.com>
2024-03-31 22:34:44 +0200

    Add teamstandings in homepage

--- f1App/src/HomePage.tsx
+++ f1App/src/HomePage.tsx
@@ -29,10 +29,23 @@ type driverStandings = {
     name: string;
   }]
 }
+type teamStandings = {
+  position: number;
+  points: number;
+  wins: number;
+  Constructor: {
+    constructorId: string;
+    name: string;
+  }
+}
 
 type DriverProp = {
   darkMode: boolean
   driver_standing: driverStandings
+}
+type TeamProp = {
+  darkMode: boolean
+  team_standing: teamStandings
 }
 function Driver_Standings_Element (props: DriverProp): React.JSX.Element {
   // import prop, to improve readability
@@ -49,12 +62,34 @@ function Driver_Standings_Element (props: DriverProp): React.JSX.Element {
         </View>
         
         <View style = {{flex: 1}}>
-          <Text style = {{fontSize: 15, fontWeight: '400'}}>{driver.givenName}</Text>
+          <Text style = {{ fontSize: 15, fontWeight: '400'}}>{driver.givenName}</Text>
           <Text style = {{fontSize: 18, fontWeight: '700', color: 'white'}}>{driver.familyName}</Text>
           <Text style = {{fontSize: 15, fontWeight: '400'}}>{standing.Constructors[0].name}</Text>
         </View>
       </View>
       <Image source={imageSource.getDriverSide(driver.familyName)} style = {{flex: 1, height: 110, width: 110, resizeMode: 'contain', alignSelf: 'flex-end'}}></Image>
+    </View>
+  )
+};
+function Team_Standings_Element (props: TeamProp): React.JSX.Element {
+  // import prop, to improve readability
+  const theme = props.darkMode ? Dark : Light;
+  
+  const team = props.team_standing.Constructor
+  const standing = props.team_standing
+  return(
+    <View style = {Styles.horizontalListElement}>
+      {/* View containing position, name and team*/}
+      <View style = {{flexDirection:'column'}}>
+        <View style = {{flex: 1}}>
+          <Text style = {{fontSize: 40, fontWeight: '800', color: 'red'}}>{standing.position}</Text>
+        </View>
+        <View style = {{flex: 1}}>
+          <Text style = {{ fontSize: 20, fontWeight: '800'}}>{team.name}</Text>
+          <Text style = {{fontSize: 13, fontWeight: '400'}}>Wins:{standing.wins}</Text>
+        </View>
+      </View>
+      <Image source={imageSource.getTeamBadge(team.constructorId)} style = {{flex: 0.5, height: 76, width: 76, resizeMode: 'contain', alignSelf: 'center'}}></Image>
     </View>
   )
 };
@@ -83,7 +118,7 @@ const HomePage = () => {
   //------ GETTING DATA FROM CACHE API --------------------------------------------
     //Hooks
     const [driver_standings_data, setDriverStanding] = useState<driverStandings[]>([]);
-
+    const [team_standings_data, setTeamStanding] = useState<teamStandings[]>([]);
     useEffect(() => {
       /*Tentativo data Caching*/
       
@@ -91,14 +126,35 @@ const HomePage = () => {
       
       setDriverStanding(driver_Cached_Data.MRData.StandingsTable.StandingsLists[0].DriverStandings);
     }, []);
+
+    
+
+    useEffect(() => {
+      /*Tentativo data Caching*/
+      
+      const team_Cached_Data : any = queryClient.getQueryData(['teamStandings']);
+      
+      setTeamStanding(team_Cached_Data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings);
+    }, []);
   //-------------------------------------------------------------------------------
   return (  
     <SafeAreaView style={[styles.safeAreaView, theme.card]}>
-      <ScrollView style = {{flex: 1}} horizontal={true}>
+      
+        <ScrollView style = {{flex: 1}} horizontal={true}>
         {driver_standings_data.map( 
           driver_standings_data => <Driver_Standings_Element key = {driver_standings_data.Driver.driverId} darkMode={darkMode} driver_standing={driver_standings_data}></Driver_Standings_Element>
         )}
+        </ScrollView>
+      
+        <View style={Styles.separator} />
+
+
+      <ScrollView style = {{flex: 1}} horizontal={true}>
+        {team_standings_data.map( 
+          team_standings_data => <Team_Standings_Element key = {team_standings_data.Constructor.constructorId} darkMode={darkMode} team_standing={team_standings_data}></Team_Standings_Element>
+        )}
       </ScrollView>
+      
       <View style={[styles.container, theme.card]}>
           <Text style={[styles.title, theme.card]}>Welcome to the Homepage</Text>
           <Button title="Go to Schedule" onPress={goToSchedule} /> 
