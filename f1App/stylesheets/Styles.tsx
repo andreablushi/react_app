8dd28a1de432c1659eb9924d8a0a076d8ff59be5
Blushi Andre <andreablushi@gmail.com>
2024-04-04 18:33:49 +0200

    Fixes

--- f1App/stylesheets/Styles.tsx
+++ f1App/stylesheets/Styles.tsx
@@ -91,8 +91,7 @@ const Styles = StyleSheet.create({
     fontFamily:'Formula1-Bold-4',
     textAlignVertical: 'center',
     textAlign:'center',
-    color: 'white'
-    
+    color: 'white',
   },
   topBar: {
     height: 70,  
@@ -128,19 +127,6 @@ const Styles = StyleSheet.create({
     textShadowOffset: {height: 1, width: 1},
     paddingVertical: 20
   },
-/*   separator:{ 
-    borderBottomWidth: 4,
-     borderBottomColor: '#5A5A5A', 
-     marginVertical: 10 
-  },
-  light_separator:{ 
-    borderBottomWidth: 4,
-    borderBottomColor: '#5A5A5A',
-    marginTop: 10,
-    marginBottom: 10,
-    marginLeft: 20,
-    marginRight: 20,
-  }, */
   header_arrow:{
     maxHeight: 25,
     maxWidth: 25, 
